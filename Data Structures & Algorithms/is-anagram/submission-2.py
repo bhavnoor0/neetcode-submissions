895906d61f1b8876f5a7648eb6256cc4e0460bad
Bhavnoor Singh <KidNamedFinguh@protonmail.com>
2026-08-10 21:14:27 +1000

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        h1 = {}
+        h2 = {}
+        for c in s:
+            h1[c] = h1.get(c, 0) + 1
+        for c in t:
+            h2[c] = h2.get(c, 0) + 1
+        
+        return h1 == h2
+
+        
